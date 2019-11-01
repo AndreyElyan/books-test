d06ca2d6a8a04fef6cffc918ed96e5ed8f0243a0
andrey <a.elyan.s@gmail.com>
2019-11-01 09:39:19 -0300

    :hammer:

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+
+import authMiddleware from './app/middlewares/auth';
+import UserController from './app/controllers/UserController';
+// import SessionController from './app/controllers/SessionController';
+import RentController from './app/controllers/RentController';
+import BookController from './app/controllers/BookController';
+
+const routes = new Router();
+
+routes.post('/users', UserController.store);
+// routes.post('/sessions', SessionController.store);
+routes.put('/users', authMiddleware, UserController.update);
+routes.post('/rents', authMiddleware, RentController.store);
+
+routes.put('/rents/:id', RentController.update);
+
+routes.get('/books', BookController.index);
+routes.get('/books/find', BookController.show);
+routes.post('/books', BookController.store);
+routes.put('/books', BookController.update);
+routes.delete('/books', BookController.delete);
+
+export default routes;
