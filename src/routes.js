44b28202c32f942488e6e3837893411e45b33891
andrey <a.elyan.s@gmail.com>
2019-11-01 17:12:10 -0300

    routes :hammer:

--- src/routes.js
+++ src/routes.js
@@ -3,22 +3,26 @@ import { Router } from 'express';
 import authMiddleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-import RentController from './app/controllers/RentController';
+import FavoriteController from './app/controllers/FavoriteController';
 import BookController from './app/controllers/BookController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
-routes.post('/sessions', SessionController.store);
 routes.put('/users', authMiddleware, UserController.update);
-routes.post('/rents', authMiddleware, RentController.store);
+routes.get('/users', authMiddleware, UserController.index);
+routes.get('/users/find', authMiddleware, UserController.show);
+routes.delete('/users', authMiddleware, UserController.delete);
 
-routes.put('/rents/:id', RentController.update);
+routes.post('/sessions', SessionController.store);
 
-routes.get('/books', BookController.index);
-routes.get('/books/find', BookController.show);
-routes.post('/books', BookController.store);
-routes.put('/books', BookController.update);
-routes.delete('/books', BookController.delete);
+routes.get('/books', authMiddleware, BookController.index);
+routes.get('/books/find', authMiddleware, BookController.show);
+routes.post('/books', authMiddleware, BookController.store);
+routes.put('/books', authMiddleware, BookController.update);
+routes.delete('/books', authMiddleware, BookController.delete);
+
+routes.post('/favorites', authMiddleware, FavoriteController.store);
+routes.put('/favorites/:id', authMiddleware, FavoriteController.update);
 
 export default routes;
