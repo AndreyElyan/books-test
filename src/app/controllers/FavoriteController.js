ea63a576dbad41609e8052de6760b70ac961bd87
andrey <a.elyan.s@gmail.com>
2019-11-02 15:24:26 -0300

    favorites :hammer:

--- src/app/controllers/FavoriteController.js
+++ src/app/controllers/FavoriteController.js
@@ -22,6 +22,7 @@ class FavoriteController {
       });
       return res.json(favorite);
     } catch (err) {
+      console.log(err);
       return res.status(500).json({ message: 'Erro Internal' });
     }
   }
--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: 'localhost',
   username: 'postgres',
   password: 'docker',
-  database: 'postgres',
+  database: 'books',
   define: {
     timestamps: true,
     underscored: true,
